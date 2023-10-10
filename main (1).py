d2371a0447694286b31ea2622c984d9384774fe5
pujan bhatt <65612957+pujan08@users.noreply.github.com>
2023-10-10 17:58:45 -0400

    Update main (1).py

--- main (1).py
+++ main (1).py
@@ -2,9 +2,9 @@
 height = input("enter your height in cm:")
 weight = input("enter your weight in kg:")
 
-a=int(weight)
-b=float(height)
-b=b/100
-bmi=a/(b*b)
+weight=int(weight)
+height=float(height)
+height=height/100
+bmi=weight/(height*height)
 answer=str(int(bmi))
 print("Your BMI is "+answer)
